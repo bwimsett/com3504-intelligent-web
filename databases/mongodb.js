0b97bd24a1b8b58192f2683cc1a101daee46eec6
Ben <b.wimsett@yahoo.co.uk>
2020-05-04 13:29:33 +0100

    Moved mongo to a single database file databases/monogodb.js, and a test story is created - db works.

--- databases/mongodb.js
+++ databases/mongodb.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 var ObjectId = require('mongodb').ObjectID;
-var bcrypt = require('bcryptjs');
+//var bcrypt = require('bcryptjs');
 
 //The URL which will be queried. Run "mongod.exe" for this to connect
 mongoose.Promise = global.Promise;
-var mongoDB = 'mongodb://localhost:27017/users';
+var mongoDB = 'mongodb://localhost:27017/com3504pwa';
 
 mongoose.Promise = global.Promise;
 
@@ -16,3 +16,4 @@ connection = mongoose.connect(mongoDB, {
     .then( console.log('connection to mongodb worked!'))
     // db.dropDatabase();
     .catch(error => console.error(error))
+
